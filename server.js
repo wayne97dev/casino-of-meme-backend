2e785c64129c901645224a8ecadd2df82076acde
Markbull <giupy997@gmail.com>
2025-04-20 23:48:25 +0200

    Remove .env from git tracking

--- server.js
+++ server.js
@@ -2014,7 +2014,7 @@ process.on('SIGTERM', async () => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 console.log(`PORT environment variable: ${process.env.PORT}`);
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
